6761834e295c458faa550e6c3000a5655c486274
Lars Kastner <lars.kastner@yahoo.dk>
2019-06-13 08:25:21 +0200

    Check that constructors return correct type

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,32 @@
 using Test
 using OscarPolytope
 
-@testset "convex_hull" begin
 pts = [1 0 0; 0 0 1]
 Q0 = convex_hull(pts)
+Q1 = convex_hull(pts, [1;1]);
+Q2 = convex_hull(pts, [1;1], [1;1]);
+C0 = cube(2)
+C1 = cube(2,1,0)
+
+@testset "conformance tests" begin
+@test typeof(Q0) == Polyhedron
+@test typeof(Q1) == Polyhedron
+@test typeof(Q2) == Polyhedron
+@test typeof(C0) == Polyhedron
+@test typeof(C1) == Polyhedron
+end
+
+@testset "convex_hull" begin
 V = vertices(Q0)
 @test V == pts 
-Q1 = convex_hull(pts, [1;1]);
 @test size(vertices(Q1)) == (2,3)
 @test size(rays(Q1)) == (2,1)
 @test size(lineality_space(Q1)) == (2,0)
-Q2 = convex_hull(pts, [1;1], [1;1]);
 @test size(vertices(Q2)) == (2,2)
 @test size(rays(Q2)) == (2,0)
 @test size(lineality_space(Q2)) == (2,1)
+end
+
+@testset "standard constructions" begin
+@test size(vertices(C0)) == (2,4)
 end
