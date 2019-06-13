bc4b69e1709855adb7bb2630d96fc62716b2878c
kalmarek <kalmar@amu.edu.pl>
2019-06-13 11:48:20 +0200

    make augment work with polymake matrices/vectors

--- src/helpers.jl
+++ src/helpers.jl
@@ -4,11 +4,11 @@ matrix_for_polymake(x::AbstractMatrix{<:Integer}) = x
 matrix_for_polymake(x::AbstractMatrix{<:Rational{<:Integer}}) = x
 matrix_for_polymake(x::Polymake.pm_MatrixAllocated{Polymake.pm_Rational}) = x
 
-augment(vec::AbstractVector{T}, val) where T = vcat(T(val), vec)
-augment(mat::AbstractMatrix, vec::AbstractVector) = hcat(vec, mat)
+augment(vec::V, val) where {T, V<:AbstractVector{T}} = V(vcat(val, vec))
+augment(mat::M, vec::AbstractVector) where {T, M<:AbstractMatrix{T}} = M(hcat(vec, mat))
 
-homogenize(::Type{T}, vec::AbstractVector, val::T=T(0)) where T = augment(T.(vec), val)
-homogenize(::Type{T}, mat::AbstractMatrix, val::T=T(1)) where T = augment(T.(mat), repeat([val], size(mat,1)))
+homogenize(::Type{T}, vec::AbstractVector, val=T(0)) where T = augment(T.(vec), val)
+homogenize(::Type{T}, mat::AbstractMatrix, val=T(1)) where T = augment(T.(mat), fill(val, size(mat,1)))
 homogenize(mat::AbstractVecOrMat{T}, val=T(1)) where T = homogenize(T, mat,
 val)
 
