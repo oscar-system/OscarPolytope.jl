6f2cc456c8129fa720ab344ee53a756e1bed2a3e
Lars Kastner <lars.kastner@yahoo.dk>
2019-06-14 10:00:25 +0200

    Indent content of testsets

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,49 +9,49 @@ C0 = cube(2)
 C1 = cube(2,1,0)
 
 @testset "conformance tests" begin
-@test typeof(Q0) == Polyhedron
-@test typeof(Q1) == Polyhedron
-@test typeof(Q2) == Polyhedron
-@test typeof(C0) == Polyhedron
-@test typeof(C1) == Polyhedron
-@test typeof(Q0 == Q0) == Bool
-@test typeof(Q0 == Q1) == Bool
-@test typeof(Q0 != Q0) == Bool
-@test typeof(Q0 != Q1) == Bool
-@test Q0 != Q1
-@test C0 != C1
-@test C0 == C0
-@test typeof(dim(Q0)) == Int
-@test typeof(ambient_dim(Q0)) == Int
-@test Q2 == convex_hull(vertices(Q2), rays(Q2), lineality_space(Q2))
+   @test typeof(Q0) == Polyhedron
+   @test typeof(Q1) == Polyhedron
+   @test typeof(Q2) == Polyhedron
+   @test typeof(C0) == Polyhedron
+   @test typeof(C1) == Polyhedron
+   @test typeof(Q0 == Q0) == Bool
+   @test typeof(Q0 == Q1) == Bool
+   @test typeof(Q0 != Q0) == Bool
+   @test typeof(Q0 != Q1) == Bool
+   @test Q0 != Q1
+   @test C0 != C1
+   @test C0 == C0
+   @test typeof(dim(Q0)) == Int
+   @test typeof(ambient_dim(Q0)) == Int
+   @test Q2 == convex_hull(vertices(Q2), rays(Q2), lineality_space(Q2))
 end
 
 @testset "convex_hull" begin
-@test size(vertices(Q0)) == (2,3)
-@test size(vertices(Q1)) == (2,3)
-@test size(rays(Q1)) == (2,1)
-@test size(lineality_space(Q1)) == (2,0)
-@test size(vertices(Q2)) == (2,2)
-@test size(rays(Q2)) == (2,0)
-@test size(lineality_space(Q2)) == (2,1)
-@test dim(Q0) == 2
-@test dim(Q1) == 2
-@test dim(Q2) == 2
-@test ambient_dim(Q0) == 2
-@test ambient_dim(Q1) == 2
-@test ambient_dim(Q2) == 2
+   @test size(vertices(Q0)) == (2,3)
+   @test size(vertices(Q1)) == (2,3)
+   @test size(rays(Q1)) == (2,1)
+   @test size(lineality_space(Q1)) == (2,0)
+   @test size(vertices(Q2)) == (2,2)
+   @test size(rays(Q2)) == (2,0)
+   @test size(lineality_space(Q2)) == (2,1)
+   @test dim(Q0) == 2
+   @test dim(Q1) == 2
+   @test dim(Q2) == 2
+   @test ambient_dim(Q0) == 2
+   @test ambient_dim(Q1) == 2
+   @test ambient_dim(Q2) == 2
 end
 
 @testset "standard constructions" begin
-@test size(vertices(C0)) == (2,4)
-@test C0 == convex_hull(vertices(C0))
+   @test size(vertices(C0)) == (2,4)
+   @test C0 == convex_hull(vertices(C0))
 end
 
 @testset "LinearProgram" begin
-LP = PrimalProgram([1;1],[1 0; 0 1;-1 0; 0 -1],[1;1;0;0])
-@test typeof(LP) == LinearProgram
-@test minimal_value(LP) == 0
-@test maximal_value(LP) == 2
-@test minimal_vertex(LP) == [0;0]
-@test maximal_vertex(LP) == [1;1]
+   LP = PrimalProgram([1;1],[1 0; 0 1;-1 0; 0 -1],[1;1;0;0])
+   @test typeof(LP) == LinearProgram
+   @test minimal_value(LP) == 0
+   @test maximal_value(LP) == 2
+   @test minimal_vertex(LP) == [0;0]
+   @test maximal_vertex(LP) == [1;1]
 end
